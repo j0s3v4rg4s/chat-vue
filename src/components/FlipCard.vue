0c8e0694364e3953643c8c72618c8dca37aabf26
jose <ruedagato@gmail.com>
2020-08-08 18:49:00 -0500

    refactor flip card

---
+++ src/components/FlipCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="content">
+    <div class="card" :class="{rotate}">
+      <div class="side">
+        <slot name="front"></slot>
+      </div>
+      <div class="side side--back">
+        <slot name="back"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "FlipCard",
+  props: {
+    rotate: Boolean,
+  },
+});
+</script>
+
+
+<style lang="scss" scoped>
+.content {
+  width: 300px;
+  margin: auto;
+  perspective: 1000px;
+
+  .card {
+    transform-style: preserve-3d;
+    transition: transform 0.8s;
+    position: relative;
+    &.rotate {
+      transform: rotateY(180deg);
+    }
+    .side {
+      width: 100%;
+      backface-visibility: hidden;
+      position: absolute;
+      z-index: 0;
+      &--back {
+        transform: rotateY(180deg);
+        z-index: 9;
+      }
+    }
+  }
+}
+</style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,51 +1,49 @@
 <template>
   <div>
     <h1 class="login-title">Welcome back</h1>
-
-    <div class="filp-card">
-      <div class="inner" :class="{ activate: !isLogin }">
-        <div class="card">
-          <v-card elevation="10">
-            <v-card-text>
-              <login />
-            </v-card-text>
-          </v-card>
-          <span class="login-option" v-if="isLogin">
-            don't have an account?
-            <v-btn text color="accent" small @click="isLogin = false">register</v-btn>
-          </span>
-        </div>
-
-        <div class="card card--back">
-          <v-card elevation="10">
-            <v-card-text>
-              <register />
-            </v-card-text>
-          </v-card>
-          <span class="login-option" v-if="!isLogin">
-            do you have a account ?
-            <v-btn text color="accent" small @click="isLogin = true">login</v-btn>
-          </span>
-        </div>
-      </div>
-    </div>
+    <flip-card :rotate="!isLogin">
+      <template v-slot:front>
+        <v-card>
+          <v-card-text>
+            <login />
+          </v-card-text>
+        </v-card>
+        <span class="login-option" v-if="isLogin">
+          don't have an account?
+          <v-btn text color="accent" small @click="isLogin = false">register</v-btn>
+        </span>
+      </template>
+      <template v-slot:back>
+        <v-card>
+          <v-card-text>
+            <register />
+          </v-card-text>
+        </v-card>
+        <span class="login-option" v-if="!isLogin">
+          do you have a account ?
+          <v-btn text color="accent" small @click="isLogin = true">login</v-btn>
+        </span>
+      </template>
+    </flip-card>
   </div>
 </template>
 
 <script lang="ts">
 import Register from "@/components/sigin/Register.vue";
 import Login from "@/components/sigin/Login.vue";
+import FlipCard from "@/components/FlipCard.vue";
 import Vue from "vue";
 
 export default Vue.extend({
   name: "SignIn",
   components: {
     Register,
-    Login
+    Login,
+    FlipCard,
   },
   data: () => ({
-    isLogin: false
-  })
+    isLogin: true,
+  }),
 });
 </script>
 
@@ -60,37 +58,5 @@ export default Vue.extend({
   color: #a5a5a5;
   display: block;
   margin-top: 10px;
-}
-
-.register-avatar {
-  display: block;
-  margin: 0 auto 10px;
-}
-
-.filp-card {
-  perspective: 1000px;
-
-  .inner {
-    position: relative;
-    transition: transform 0.8s;
-    transform-style: preserve-3d;
-    width: 300px;
-    margin: 20px auto 0;
-    transform-origin: center right;
-
-    .card {
-      //position: absolute;
-      backface-visibility: hidden;
-      width: 100%;
-
-      &--back {
-        transform: rotatey(180deg);
-      }
-    }
-
-    &.activate {
-      transform: translateX(-100%) rotateY(-180deg);
-    }
-  }
 }
 </style>
